5d25d5de99f0c56d74bd74406e241708fd09cc0a
crayfish007 <crayfish007@protonmail.com>
2021-05-18 22:19:33 +0800

    fix: Remove async

--- api/nft/punk.ts
+++ api/nft/punk.ts
@@ -2,12 +2,12 @@ import { NowRequest, NowResponse } from "@vercel/node";
 import punksJson from "./config/punks.json";
 import { PUNK_TYPES, ALL_ACCESSORIES } from "./config/type";
 
-export const getPunkMeta = async (punkId: string): Promise<any> => {
+export const getPunkMeta = (punkId: string) => {
   if (+punkId < 0 || +punkId > 9999) {
     throw new Error("PunkIndex out of range");
   }
   // @ts-ignore
-  const rawPunk = punksJson[punkIndex];
+  const rawPunk = punksJson[punkId];
   return {
     punkIndex: punkId,
     type: PUNK_TYPES[rawPunk.t],
